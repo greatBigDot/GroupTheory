c38dd9b42b77835ae98cc98246fc59b01144f71c
John Michael @R7 <figueroa.johnm@gmail.com>
2017-02-08 15:12:28 -0500

    Fix minor syntax errors; replace tabs with spaces; compile to .exe; successful printing of "Hello World"

--- Group.hs
+++ Group.hs
@@ -1,15 +1,14 @@
-data Group a = Group
-{ set      :: [a]
+data Group a = Group {
+  set      :: [a]
 , func     :: (a -> a -> a)
 , identity :: a
-, inverse  :: a -> a
-}
+, inverse  :: a -> a}
 
 showGroup :: (Show a) => Group a -> String 
 showGroup grp = show (set grp)
 
 isValidGroup :: (Eq a) => Group a -> Bool
-isValidGroup = foldl1 (&&) (map (\f -> f grp) [isClosed, isIdentityIdentity, isInverseInverse, isAssociative])
+isValidGroup grp = foldl1 (&&) (map (\f -> f grp) [isClosed, isIdentityIdentity, isInverseInverse, isAssociative])
 
 
 isClosed :: (Eq a) => Group a -> Bool
@@ -22,7 +21,7 @@ isInverseInverse :: (Eq a) => Group a -> Bool
 isInverseInverse grp = foldl (&&) True (map (iInIn grp) (set grp))
 
 isAssociative :: (Eq a) => Group a -> Bool
-isAssociative = foldl (&&) True (map (iA grp) (cCube (set grp)))
+isAssociative grp = foldl (&&) True (map (iA grp) (cCube (set grp)))
 
 iC :: (Eq a) => Group a -> (a, a) -> Bool
 iC grp (g,h) = elem (f g h) (set grp)
@@ -31,13 +30,13 @@ iC grp (g,h) = elem (f g h) (set grp)
 iIdId :: (Eq a) => Group a -> a -> Bool
 iIdId grp g = (f g e) == g && (f e g) == g
   where f = func grp
-	e = identity grp
+        e = identity grp
 
 iInIn :: (Eq a) => Group a -> a -> Bool
-iInIn grp g = (f g h) == e && (f h g) == e
+iInIn grp g = (f g (inv g)) == e && (f (inv g) g) == e
   where f = func grp
-	e = identity grp
-	h = inverse grp
+        e = identity grp
+        inv = inverse grp
 
 iA :: (Eq a) => Group a -> (a,a,a) -> Bool
 iA grp (g,h,j) = f (f g h) j == f g (f h j)
